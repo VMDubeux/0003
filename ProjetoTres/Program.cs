bd898011566998374efaf0d415c0cc5d5b9b6616
Victor Dubeux <adv.victordubeux@gmail.com>
2023-02-03 20:03:33 -0300

    P3 C3 - Envio definitivo de projeto

--- ProjetoTres/Program.cs
+++ ProjetoTres/Program.cs
@@ -7,14 +7,21 @@ namespace Exercicio
     {
         static void Main(string[] args)
         {
-
+            Console.WriteLine("Digite um número inteiro pequeno: ");
             sbyte numero1 = sbyte.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um único caracter: ");
             char letra1 = char.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um número real: ");
             float numero2 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Escreva algo: ");
             string nome = Console.ReadLine();
+            Console.WriteLine("Digite outro caracter: ");
             char letra2 = char.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número inteiro pequeno: ");
             sbyte numero3 = sbyte.Parse(Console.ReadLine());
+            Console.WriteLine("Digite novamente um número real: ");
             float numero4 = float.Parse(Console.ReadLine());
+            Console.WriteLine("Digite um nome, um único caracter, um número inteiro pequeno, um número real, sempre com um espaço entre eles: ");
             string[] v = Console.ReadLine().Split(' ');
             string nome2 = v[0];
             char letra3 = char.Parse(v[1]);
@@ -33,6 +40,8 @@ namespace Exercicio
             Console.WriteLine(letra3);
             Console.WriteLine(numero5);
             Console.WriteLine(numero6.ToString("F2", CultureInfo.InvariantCulture));
+
+            Console.ReadKey();
         }
     }
 }
